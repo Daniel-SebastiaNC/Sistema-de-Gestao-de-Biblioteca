bb5c683fd49dd7b766c10b49a02086466227b93a
Daniel-SebastiaNC <daniel.sebastian.nc@gmail.com>
2026-08-13 16:36:52 -0300

    chore: add EntityFrameworkCore package (SQLite and Design)

--- obj/Debug/net10.0/Biblioteca.Api.AssemblyInfo.cs
+++ obj/Debug/net10.0/Biblioteca.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Biblioteca.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6426924e38fda7eed2f1096e852d694d7ddebaa2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac5c368c945af9eab31a20f765c280b32782a2ee")]
 [assembly: System.Reflection.AssemblyProductAttribute("Biblioteca.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Biblioteca.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
